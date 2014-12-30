c5ff1c79e01cbfa2006af4410ee74fbb4ae7e541
PatrickG <patrick.guan@mail.utoronto.ca>
2014-12-30 17:00:48 -0500

    added the navbars and linked creating an event

--- website/events/urls.py
+++ website/events/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from events import views
+from events import views, eventview
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
 	url(r'^register/', views.register, name='register'),
 	url(r'^register_user/', views.register_user, name='register_user'),
 	url(r'^search/', views.search, name='search'),
+	url(r'^create/', eventview.create, name='create'),
 	)
